fc25c2e7adbb3c46c0d5e31d8854efe098c21b95
hamza210307 <hamzafac2007@gmail.com>
2023-03-07 06:44:13 +0500

    myportfolio

---
+++ script.js
@@ -0,0 +1,13 @@
+var hamburger = document.querySelector(".hamb")
+var navlist = document.querySelector(".nav-list")
+var link = document.querySelector(".nav-list li")
+
+hamburger.addEventListener("click", function(){
+    this.classList.toggle("click");
+    navlist.classList.toggle("open")
+})
+
+
+// let poll = {
+//     question:"Do You like My Portfolio"
+// }
